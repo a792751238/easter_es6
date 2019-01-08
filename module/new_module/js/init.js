8d3673579dfc494751c68beeddcd984692489780
easterCat <fuhuodemao@163.com>
2019-01-08 19:51:05 +0800

    jp

--- module/new_module/js/init.js
+++ module/new_module/js/init.js
@@ -1,30 +1,13 @@
-// ki.setConfig({
-//     baseUrl: "lib/",
-//     paths: {
-//         jquery: "jquery.js",
-//         underscore: "underscore.js"
-//     }
-// });
+ki.setConfig({
+    baseUrl: "lib/",
+    paths: {
+        jquery: "jquery.js",
+        underscore: "underscore.js"
+    }
+});
 
-// vk.use(["jquery"], function() {
-//     // rgba(0,0,0);
-//     var color = ["rgba(", Math.floor(Math.random() * 255), ",", Math.floor(Math.random() * 255), ",", Math.floor(Math.random() * 255), ")"];
-
-//     setTimeout(function() {
-//         $(".background").css({
-//             position: "fixed",
-//             top: "0px",
-//             bottom: "0px",
-//             left: "0px",
-//             right: "0px",
-//             background: color.join("")
-//         });
-//     }, 3000);
-// });
-
-vk.use(["jquery", "underscore"], function() {
+ki.depends(["jquery", "underscore"], function() {
     // rgba(0,0,0);
-    alert(123);
     var color = ["rgba(", Math.floor(Math.random() * 255), ",", Math.floor(Math.random() * 255), ",", Math.floor(Math.random() * 255), ")"];
 
     $(".background").css({
--- module/new_module/lib/module.js
+++ module/new_module/lib/module.js
@@ -1,94 +1,97 @@
-(function() {
-    var vk_config = {
-        root: "./lib/",
-        path: {
-            jquery: "jquery.js",
-            underscore: "underscore.js"
-        }
-    };
+(function(win) {
+    var baseUrl;
+    var paths;
+    var script_cache = {};
+    var script_queue = [];
 
-    var vk = {
-        loadjs: function(script_filename, callback) {
-            script_doc = document.getElementById(script_filename);
-            if (script_doc) {
-                return;
-            }
-            var script = document.createElement("script");
-            script.setAttribute("id", script_filename);
-            script.setAttribute("type", "text/javascript");
-            script.setAttribute("src", vk_config.root + vk_config.path[script_filename]);
-            var fs = document.getElementsByTagName("script")[0];
-            fs.parentNode.insertBefore(script, fs);
-            console.log("loading:" + script_filename);
-            script.onload = script.onreadystatechange = function() {
-                console.log("loaded:" + script_filename);
-                callback();
+    var setConfig = function(options) {
+        if (!options) {
+            options = {
+                baseUrl: "/",
+                paths: {}
             };
-        },
-        count_js: 0,
-        use: function(ids, callback) {
-            var that = this;
-            if (!Array.isArray(ids)) {
-                ids = [ids];
-            }
-            that.count_js = ids.length;
-            for (var i = 0; i < ids.length; i++) {
-                (function(i) {
-                    script_doc = document.getElementById(ids[i]);
-                    if (script_doc) {
-                        return;
-                    }
-                    script = document.createElement("script");
-                    script.setAttribute("id", ids[i]);
-                    script.setAttribute("type", "text/javascript");
-                    script.setAttribute("src", vk_config.root + vk_config.path[ids[i]]);
-                    var fs = document.getElementsByTagName("script")[0];
-                    fs.parentNode.insertBefore(script, fs);
-                    console.log("loading:" + ids[i]);
-                    script.onload = script.onreadystatechange = function() {
-                        console.log("loaded:" + ids[i]);
-                        that.count_js--;
-                        if (that.count_js == 0) {
-                            callback();
-                        }
-                    };
-                })(i);
-            }
-        },
-        seq: function(ids, callback) {
-            var that = this;
-            if (!Array.isArray(ids)) {
-                ids = [ids];
-            }
-            that.count_js = ids.length;
-            function load(script_filename) {
-                return new Promise(function(resolve, reject) {
-                    script_doc = document.getElementById(script_filename);
-                    if (script_doc) {
-                        return;
-                    }
-                    var script = document.createElement("script");
-                    script.setAttribute("id", script_filename);
-                    script.setAttribute("type", "text/javascript");
-                    script.setAttribute("src", vk_config.root + vk_config.path[script_filename]);
-                    document.getElementsByTagName("body")[0].appendChild(script);
-                    console.log("loading:" + script_filename);
-                    script.onload = script.onreadystatechange = function() {
-                        console.log("loaded:" + script_filename);
-                        resolve("loaded:" + script_filename);
-                    };
-                    script.onerror = function() {
-                        reject(new Error("fail to load"));
-                    };
-                });
-            }
+        }
+        if (options.baseUrl) baseUrl = options.baseUrl;
+        if (options.paths) paths = options.paths;
+    };
 
-            var promise = Promise.resolve();
-            for (let i = 0; i < ids.length; i++) {
-                promise.then(load(ids[i]));
+    // 用来创建script标签并且添加到body上
+    var _createScript = function(script_name) {
+        var scrpit_element = document.createElement("script");
+        var fs = document.getElementsByTagName("script")[0];
+
+        scrpit_element.setAttribute("id", script_name);
+        scrpit_element.setAttribute("type", "text/javascript");
+        scrpit_element.setAttribute("src", baseUrl + paths[script_name]);
+
+        fs.parentNode.insertBefore(scrpit_element, fs);
+
+        return scrpit_element;
+    };
+
+    var _saveScript = function(script) {
+        script.status = "loaded";
+        script_cache[script.name] = script;
+    };
+
+    var _queue = function() {
+        var save_script = script_queue[0];
+
+        if (script_queue[0]) {
+            var script = document.getElementById(script_queue[0].name);
+
+            if (script) {
+                if (script_queue.length > 0) {
+                    script_queue.splice(0, 1);
+                    _queue();
+                } else {
+                    return script_queue[0].callback();
+                }
+            } else {
+                var script = _createScript(script_queue[0].name);
+
+                script.onload = script.onreadystatechange = function() {
+                    _saveScript(script_queue[0]);
+                    script_queue.splice(0, 1);
+                    if (script_queue.length <= 0) {
+                        save_script.callback();
+                    } else {
+                        _queue();
+                    }
+                };
             }
+        } else {
+            return;
         }
     };
 
-    window.vk = vk;
-})();
+    var depends = function(script_names, callback) {
+        if (Object.prototype.toString.call(script_names) !== "[object Array]") {
+            if (script_names) {
+                script_names = [script_names];
+            } else {
+                script_names = [];
+            }
+        }
+
+        script_names.forEach(function(item) {
+            script_queue.push({
+                name: item,
+                status: "start",
+                callback: callback
+            });
+        });
+
+        _queue();
+    };
+
+    var getCache = function() {
+        return script_cache;
+    };
+
+    win.ki = {
+        getCache,
+        setConfig,
+        depends
+    };
+})(window);
